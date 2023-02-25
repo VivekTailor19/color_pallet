8873ba7d938160182efe7f2fcb96964854e177b7
Vivek <tailorvivekgec19@gmail.com>
2023-02-25 00:36:24 -0800

    Unique Colors generate from list in Color Pallet

---
+++ lib/color_pallet_random.dart
@@ -0,0 +1,196 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class Color_Pallet_Random extends StatefulWidget {
+  const Color_Pallet_Random({Key? key}) : super(key: key);
+
+  @override
+  State<Color_Pallet_Random> createState() => _Color_Pallet_RandomState();
+}
+List col = [];
+
+int m = 0, n = 0 ;
+int a=0,b=0,c=0,d=0,e=0,f=0;
+
+
+int? num ;
+
+List temp = [];
+List colors = [
+  Colors.blue,
+  Colors.white,
+  Colors.black,
+  Colors.grey,
+  Colors.redAccent,
+  Colors.greenAccent,
+  Colors.amberAccent,
+  Colors.pinkAccent,
+  Colors.purpleAccent,
+  Colors.deepPurpleAccent,
+  Colors.orangeAccent,
+  Colors.deepOrangeAccent
+];
+
+class _Color_Pallet_RandomState extends State<Color_Pallet_Random> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Color(0xffD8E6F4),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                        text: "Color Palette\n",
+                        style: TextStyle(fontSize: 25, color: Color(0xff3BB3F9))),
+                    TextSpan(
+                        text: "Generator",
+                        style: TextStyle(fontSize: 25, color: Color(0xff3BB3F9))),
+                  ],
+                ),
+              ),
+              Container(
+                width: 80,
+                height: 300,
+
+                decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)),color: Colors.teal,),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Pallet(colors[a]),
+                      Pallet(colors[b]),
+                      Pallet(colors[c]),
+                      Pallet(colors[d]),
+                      Pallet(colors[e]),
+                      Pallet(colors[f]),
+                    ],
+                  ),
+                ),
+              ), //Color Pallet
+              GestureDetector(onTap: () {
+                setState(() {
+                  Random rand = Random();
+                  a = rand.nextInt(12);
+
+                  b = rand.nextInt(12);
+                  while(b == a)
+                    {
+                      b = rand.nextInt(12);
+                    };
+
+                  c = rand.nextInt(12);
+                  while(c == a || c==b)
+                  {
+                    c = rand.nextInt(12);
+                  };
+
+                  d = rand.nextInt(12);
+                  while(d == a || d==b || d==c)
+                  {
+                    d = rand.nextInt(12);
+                  };
+
+                  e = rand.nextInt(12);
+                  while(e == a || e==b || e==c || e==d)
+                  {
+                    e = rand.nextInt(12);
+                  };
+
+                  f = rand.nextInt(12);
+                  while(f == a || f==b || f==c || f==d || f==e)
+                  {
+                    f = rand.nextInt(12);
+                  };
+                  temp = [a,b,c,d,e,f];
+
+
+                  // for(int g = 0 ; g<6 ; g++)
+                  // {
+                  //   colors[m]=temp[n];
+                  // }
+                  //
+
+                  print ("$temp $a $b $c $d $e $f");
+
+
+                });
+
+
+
+              },
+
+                child: Container(
+                  height: 50,
+                  width: 250,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                        Radius.circular(15)),
+                    border: Border.all(color: Color(0xff3BB3F9)),
+                  ),
+
+                  child: Text("Generate",
+                      style: TextStyle(fontSize: 25, color: Color(0xff3BB3F9))),
+                ),
+              ), //    Generate
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget Pallet(Color c1)
+  {
+    return Expanded(
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        color: c1,
+      ),
+    );
+  }
+
+}
+
+/*
+
+generate random num
+num = col first index
+
+generate random num
+num != col second index
+
+generate random num
+num != col first second index
+
+for(int i = 0 ; i<colors.length ; i++)
+{
+    random color 1
+    print 1 --> pallet 1
+
+    random color 2
+    color 2 != color 1
+    print 2 -->  pallet 2
+
+    random color 3
+    color 3 != color 1 !=color 2
+    print 3 --> pallet 3
+
+    .
+    .
+    .
+    .
+    .
+
+
+}
+
+
+
+
+* */
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+import 'package:color_pallet/color_pallet_random.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Color_Pallet_Random(),
+    ),
+  );
+}
